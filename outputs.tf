8c984e579904bfc612cd2f1114f8365db3341f98
jbennett7 <joe.bennett@contino.io>
2019-09-11 14:29:22 -0700

    updates outputs.tf with some more outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
 output "subnetworks" {
   value = module.vpc.subnets_self_links
 }
+
+output "hosted_project_id" {
+  value = module.host-project.project_id
+}
+
+output "org_id" {
+  value = var.org_id
+  sensitive = true
+}
+
+output "billing_account" {
+  value = var.billing_account
+  sensitive = true
+}
